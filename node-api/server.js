6483f8a72799ae19097c823498f6521c81613d5c
Elias Bernardo <ebmm01@gmail.com>
2019-10-19 21:42:44 -0300

    Connecting server to mongoDB

--- node-api/server.js
+++ node-api/server.js
@@ -1,6 +1,14 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
+// Iniciando o app
 const app = express();
+
+// Iniciando o DB
+mongoose.connect('mongodb://localhost:27017/nodeapi', { 
+    useNewUrlParser: true ,
+    useUnifiedTopology: true,
+})
 
 app.get('/', (req, res) => {
     res.send('Hello world, esse é um teste =)');
